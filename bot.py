5fc1c9b7d34abf784129bc4e78b3b463afca1bd2
commonsensicaI <100944274+commonsensicaI@users.noreply.github.com.>
2026-04-23 03:50:39 +0200

    add start command

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
+import asyncio
 import os
 import subprocess
+import time
 
 import discord
+import paramiko
+import psutil
 from discord.ext import commands
 from dotenv import load_dotenv
+from wakeonlan import send_magic_packet
 
 load_dotenv(".env")  # charge le fichier .env
 
@@ -11,6 +16,9 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 
 @bot.event
@@ -26,7 +34,29 @@ async def ping(ctx):
 @bot.command()
 async def start(ctx):
     await ctx.send("Démarrage du serveur...")
-    subprocess.Popen(["bash", "/home/emma/minecraft-server/start.sh"])
+    connecte = False
+    try:
+        ssh.connect("192.168.1.201", username="emma", key_filename="")
+        connecte = True
+    except:
+        await ctx.send("Serveur éteint, il va s'allumer")
+
+    if connecte:
+        stdin, stdout, stderr = ssh.exec_command("pgrep java")
+        output = stdout.read().decode().strip()
+        if output:  # si y'a un PID c'est que java tourne
+            await ctx.send("Le serveur est déjà lancé !")
+            return
+
+    send_magic_packet("58:11:22:cd:d5:c5")
+    await asyncio.sleep(60)
+    try:
+        ssh.connect("192.168.1.201", username="emma", key_filename="")
+    except:
+        await ctx.send("Contact Emma, she screwed up")
+        return
+
+    ssh.exec_command("bash /home/emma/minecraft-server/start.sh")
     await ctx.send("Serveur lancé !")
 
 
